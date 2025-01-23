f6d5e3876994ec983d5ef9ff9caf23b0a87f9120
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2025-01-23 16:55:16 +0200

    add cookies

---
+++ theme/js/cookie.js
@@ -0,0 +1,5 @@
+function setCookie(name,value,days){var date=new Date();date.setTime(date.getTime()+(days*24*60*60*1000));var expires="expires="+date.toUTCString();document.cookie=name+"="+value+";"+expires+";path=/";}
+function getCookie(name){var nameEQ=name+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i].trim();if(c.indexOf(nameEQ)===0)return c.substring(nameEQ.length,c.length);}
+return null;}
+window.onload=function(){if(getCookie('cookies-accepted')==='true'){document.getElementById('cookie-banner').style.display='none';}else{document.getElementById('cookie-banner').style.display='block';}
+document.getElementById('accept-cookies').addEventListener('click',function(){setCookie('cookies-accepted','true',365);document.getElementById('cookie-banner').style.display='none';});};
